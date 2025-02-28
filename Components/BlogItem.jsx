7a668fd471d21f77ad04fe5cdf7e203395aea528
Sivagnanalingam Yathavan <yathava-se20015@stu.kln.ac.lk>
2025-02-28 11:27:38 +0530

    frontend

---
+++ Components/BlogItem.jsx
@@ -0,0 +1,24 @@
+import { assets, blog_data } from '@/Assets/assets'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const BlogItem = ({image,category,title,description,id}) => {
+  return (
+    <div className='flex flex-col justify-center max-w-[330px] am:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]'>
+      <Link href={`/blogs/${id}`}>
+        <Image src={image} alt='' width={400} height={400} className='border-b border-black' />
+      </Link>  
+      <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-sm'>{category}</p>
+        <div className="p-5">
+            <h5 className='mb-t text-lg font-medium tracking-tight text-grey-900'>{title}</h5>
+            <p className='mb-3 text-sm tracking-tight text-grey-700'>{description}</p>
+            <Link href={`/blogs/${id}`} className='inline-flex items-center py-2 font-semibold text-center'>
+                Read more <Image src={assets.arrow} className='ml-2' alt='' width={12}/>
+            </Link>
+        </div>
+    </div>
+  )
+}
+
+export default BlogItem
